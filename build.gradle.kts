7c000f2f818cb2135139de50018ec88f60a8c41f
Danilo Pianini <danilo.pianini@unibo.it>
2024-02-27 15:08:24 +0100

    ci: add the OOP machinery

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,13 @@
  * For more details take a look at the 'Building Java & JVM projects' chapter in the Gradle
  * User Manual available at https://docs.gradle.org/8.0.2/userguide/building_java_projects.html
  */
-
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
+
+    id("org.danilopianini.unibo-oop-gradle-plugin") version "1.0.6"
 }
+
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -40,3 +42,4 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = "buontyhunter.App"
     }
 }
+ java { toolchain { languageVersion.set(JavaLanguageVersion.of(17)) } }
